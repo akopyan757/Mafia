30b6f294799fd1cb8849735778458c27062a9a46
Albert Akopyan <akopyan757@gmail.com>
2024-03-22 00:12:55 +0400

    Updated kotlin version

--- composeApp/src/desktopMain/kotlin/com/cheesecake/mafia/common/Resources.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/cheesecake/mafia/common/Resources.desktop.kt
@@ -2,11 +2,12 @@ package com.cheesecake.mafia.common
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
+import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 actual fun imageResources(imageRes: String): Painter {
-    return painterResource("drawable/$imageRes")
+    return painterResource(resource = DrawableResource("drawable/$imageRes"))
 }
